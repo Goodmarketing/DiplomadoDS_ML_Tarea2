77ed08b8e7ec837b4dc03c8349bc00818abcd4e4
Iván Vázquez <108256880+Goodmarketing@users.noreply.github.com>
2022-07-09 23:39:16 -0500

    Update exercise.py

--- assignments/04_16CuentaInversion/src/exercise.py
+++ assignments/04_16CuentaInversion/src/exercise.py
@@ -1,8 +1,22 @@
-# Escribe tus funciones abajo de esta línea
-
+def calculo_interes(cantidad,interes):
+    if cantidad > 0 and interes > 0:
+        for i in range(1,13,1):
+            cantidad = cantidad + (cantidad * ((interes/12)/100))
+        return cantidad
+    elif cantidad <= 0 or interes <= 0:
+        cantidad = 'Error en los datos'
+        return cantidad
+         
 def main():
-    # Escribe tu código abajo de esta línea
-    pass
+    cantidad = int(input("Escribe la cantidad de dinero inicial : "))
+    interes = int(input("Escribe el porcentaje de interes anual : "))
+
+    x = calculo_interes(cantidad,interes)
+    if x != 'Error en los datos':
+        x = "{:.2f}".format(x)
+        print(x)
+    else: 
+        print(x)
 
 if __name__ == '__main__':
     main()
