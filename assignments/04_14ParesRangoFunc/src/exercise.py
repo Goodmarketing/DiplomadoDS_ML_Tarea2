5b6ecf8970295ac08025f78f08c1448a2f2902ed
Iván Vázquez <108256880+Goodmarketing@users.noreply.github.com>
2022-07-09 23:40:34 -0500

    Update exercise.py

--- assignments/04_14ParesRangoFunc/src/exercise.py
+++ assignments/04_14ParesRangoFunc/src/exercise.py
@@ -1,8 +1,28 @@
-# Escribe tus funciones abajo de esta línea
-
+def pares_en_rango(inf,sup):
+    if inf == sup:
+        if inf % 2 == 0:
+            print(inf)
+        elif inf % 2 != 0:
+            print('No hay pares')
+    if inf != sup and inf < sup:
+        if inf % 2 == 0:
+            for i in range(inf,sup+1,2):
+                print(f'{i}')
+        elif inf % 2 != 0:
+            for i in range(inf+1,sup+1,2):
+                print(f'{i}')
+    if sup < inf:
+        if sup % 2 == 0:
+            for i in range(sup,inf+1,2):
+                print(f'{i}')
+        elif sup % 2 != 0:
+            for i in range(sup+1,inf+1,2):
+                print(f'{i}')
+    
 def main():
-    # Escribe tu código abajo de esta línea
-    pass
+    inf = int(input("Valor 1: "))
+    sup = int(input("Valor 2: "))
+    pares_en_rango(inf,sup)
 
 if __name__ == '__main__':
     main()
