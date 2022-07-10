f48df869a7af6ddc5b19d6a59736ce03ddd99772
Iván Vázquez <108256880+Goodmarketing@users.noreply.github.com>
2022-07-09 23:40:00 -0500

    Update exercise.py

--- assignments/04_15Primos/src/exercise.py
+++ assignments/04_15Primos/src/exercise.py
@@ -1,8 +1,21 @@
-# Escribe tus funciones abajo de esta línea
+def primo(num):
+    if num < 0:
+        return False
+    if num == 1:
+        return False
+    if num == 2:
+        return True
+    if num > 2:
+        for i in range(2,num):
+            if num % i > 0:
+                return True
+            else:
+                return False
 
 def main():
-    # Escribe tu código abajo de esta línea
-    pass
+    num = int(input("Escribe un numero entero : "))
+    x = primo(num)
+    print(x)
 
 if __name__ == '__main__':
     main()
